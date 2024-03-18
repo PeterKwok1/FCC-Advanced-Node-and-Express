13e3ef58b71bc6f76cab6acdd740f8627ff025df
Peter Kwok <peter.kwok94@gmail.com>
2024-03-18 18:45:35 -0400

    feat: 13/22

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const session = require('express-session')
 const passport = require('passport')
 const { ObjectID } = require('mongodb')
 const LocalStrategy = require('passport-local')
+const bcrypt = require('bcrypt')
+const routes = require('./routes.js')
+const auth = require('./auth.js')
 
 app.set('view engine', 'pug')
 app.set('views', './views/pug')
@@ -30,74 +33,14 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { // checks if req.user is defined 
-    return next()
-  }
-  res.redirect('/')
-}
-
 // wrapping routes and serialization prevents users from making requests before the database is connected.
 // param is a callback which takes a client which is created within the function (see connection.js)
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users')
 
-  app.route('/').get((req, res) => {
-    res.render('index', {
-      showLogin: true,
-      title: 'Connected to Database',
-      message: 'Please login'
-    })
-  })
+  routes(app, myDataBase)
+  auth(app, myDataBase)
 
-  app.route('/login').post(
-    passport.authenticate('local', { failureRedirect: '/' }),
-    (req, res) => {
-      res.redirect('/profile')
-    }
-  )
-
-  app.route('/profile').get(
-    ensureAuthenticated,
-    (req, res) => {
-      res.render('profile', {
-        username: req.user.username
-      })
-    }
-  )
-
-  app.route('/logout').get(
-    (req, res) => {
-      req.logout()
-      res.redirect('/')
-    }
-  )
-
-  app.use((req, res, next) => {
-    res.status(404)
-      .type('text')
-      .send('Not Found')
-  })
-
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`)
-      if (err) { return done(err) }
-      if (!user) { return done(null, false) }
-      if (password !== user.password) { return done(null, false) }
-      return done(null, user)
-    })
-  }))
-
-  passport.serializeUser((user, done) => {
-    done(null, user._id)
-  })
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc)
-    })
-  })
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' })
@@ -105,8 +48,6 @@ myDB(async client => {
 })
 
 //
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
